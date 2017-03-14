9c6a48dcd5eb6f43043e131b749818f57870f4cb
Arinnka <Delena315@yandex.ru>
2017-03-14 05:19:52 +0300

    lab2

--- lab2-1.php
+++ lab2-1.php
@@ -4,8 +4,7 @@
    $word = "abcdefghi";
         for($i = 0;  $i < 9; $i++){           
             echo $word.'<br />';
-            //Убираем первый символ
-            $word = substr($word,1,-1);//
+            $word = substr($word,1,-1);
           
         }
 
