f708f08c50a77182380edd277694191e3b49f10e
Arinnka <Delena315@yandex.ru>
2017-03-14 04:59:47 +0300

    lab2

---
+++ form2.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<form action="lab2.php" method="post">
+ <p>Добавление нового товара</p>
+ <p>Товар = <input type="text" name="flower" /></p>
+ <p>Цена = <input type="text" name="price" /></p>
+ <p><input type="submit" /></p>
+</form>
+</body>
+</html>
---
+++ lab2.php
@@ -0,0 +1,44 @@
+<?php
+	 $arr = array(
+                "tulip" => 150,
+                "rose" => 25,
+                "aster" => 37,
+            ); 
+            $arr["aloe"] = 140;
+            $arr["iris"] = 89;
+			
+	echo ('Товары:<br />');
+		
+	$flower = (string)$_POST['flower'];
+	$price = (double)$_POST['price'];
+	
+	if ($flower!=='' and $price!='')
+	{
+	$arr = $arr+array("$flower"=>$price);
+	}
+	
+	
+	foreach($arr as $key=>$value)
+	{
+		 echo $key." - ".$value.'<br />';
+		echo('<br />');
+	}
+	echo('<br />');
+	
+	
+	echo "Количество товаров ".sizeof($arr).'<br />';
+	echo "Сумма товаров ".array_sum($arr).'<br />';
+	
+	
+	asort($arr);
+	echo ('Сортировка<br />');	
+	foreach($arr as $key=>$value)
+	{
+		 echo $key." - ".$value.'<br />';
+		echo('<br />');
+	}
+	echo('<br />');
+	
+
+
+?>
